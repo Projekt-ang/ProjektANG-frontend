33a2239215c64c4a9a3e92e52a932ef3243f0d6f
dragranis <dragfirm2@gmail.com>
2019-10-30 11:11:35 +0100

    małe zmiany strony głównej

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,6 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
 
-    <button @click="testApi()">API test</button>
     <p v-if="content">{{this.content}}</p>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
