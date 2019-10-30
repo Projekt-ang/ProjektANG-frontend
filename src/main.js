59352098305f0c542292af55561d810821b4262b
karolina1037 <47772211+karolina1037@users.noreply.github.com>
2019-10-30 22:03:32 +0100

    instalacja vue2 + global

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 /*jshint node: true, esversion: 6 */
 import Vue from 'vue';
+import Vue2Editor from "vue2-editor";
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -18,6 +19,7 @@ Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(VueLodash);
 Vue.use(VueModal);
+Vue.use(Vue2Editor);
 
 new Vue({
   router,
