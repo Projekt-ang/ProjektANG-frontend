d7295b1335a3677235a55fe6c0ecf0c8f79ec1d6
ksikorski1 <axfitey@gmail.com>
2020-01-07 23:23:00 +0100

    usuwanie

--- src/components/modals/ConfirmDelete.vue
+++ src/components/modals/ConfirmDelete.vue
@@ -1,23 +1,23 @@
 <template>
-<div id="confirmDelete" class="modal fade" role="dialog">
-        <div class="modal-dialog w-50">
-          <div class="modal-content">
-            <div class="modal-header">
-              <a class="modal-title font-weight-bold text-center">Czy jesteś pewien? {{id}}</a>
-              <button type="button" class="close" data-dismiss="modal">&times;</button>
-            </div>
-            <div class="modal-footer justify-content-center">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Anuluj</button>
-              <button
-                type="submit"
-                class="btn btn-danger"
-                data-dismiss="modal"
-                @click="usun(id)"
-              >Usuń</button>
-            </div>
+  <div id="confirmDelete" class="modal fade" role="dialog">
+    <div class="modal-dialog w-50">
+      <div class="modal-content">
+        <div class="modal-header">
+          <a class="modal-title font-weight-bold text-center">Czy jesteś pewien? {{id}}</a>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+          <div class="modal-footer justify-content-center">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Anuluj</button>
+            <button
+              type="submit"
+              class="btn btn-danger"
+              data-dismiss="modal"
+              @click="usun(id)"
+            >Usuń</button>
           </div>
         </div>
       </div>
+    </div>
 </template>
 
 <script>
