b1b09fb1ab3cf4a4f78fb6208078ea5c0394fb4b
dragranis <dragfirm2@gmail.com>
2020-02-05 16:45:02 +0100

    widok results dla użytkownika

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -7,10 +7,17 @@
       <table table class="table table-striped table-hover table-bordered w-50 mx-auto">
         <thead>
           <th>Test</th>
-          <th>Wynik</th>
+          <th>Punkty</th>
+          <th>Max. Punktów</th>
+          <th>Wynik(%)</th>
         </thead>
         <tbody>
-          <tr></tr>
+          <tr v-for="(test,testId) in results" :key="testId">
+            <td>{{testNames[testId]}}</td>
+            <td>{{test.points}}</td>
+            <td>{{test.maxPoints}}</td>
+            <td>{{test.percentage}}</td>
+          </tr>
         </tbody>
       </table>
     </div>
@@ -22,7 +29,8 @@ export default {
   name: "Results",
   data() {
     return {
-      results: []
+      results: [],
+      testNames: []
     };
   },
   methods: {},
@@ -30,6 +38,15 @@ export default {
     this.$req.get("/api/get-user-results/" + this.user.id).then(response => {
       this.results = response.data._embedded;
       console.log(this.results);
+
+      for (var i in this.results) {
+        if (this.results[i].blankInsertTest != null) {
+          this.testNames.push(this.results[i].blankInsertTest.name);
+        }
+        if (this.results[i].readingVideoTest != null) {
+          this.testNames.push(this.results[i].readingVideoTest.name);
+        }
+      }
     });
   },
   computed: {
